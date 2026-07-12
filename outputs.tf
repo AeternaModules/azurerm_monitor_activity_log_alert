0226857cdb456cf45ac5d6e7fe350dd5c8e22247
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:53 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "monitor_activity_log_alerts" {
-  description = "All monitor_activity_log_alert resources"
-  value       = azurerm_monitor_activity_log_alert.monitor_activity_log_alerts
-}
 output "monitor_activity_log_alerts_action" {
-  description = "List of action values across all monitor_activity_log_alerts"
-  value       = [for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : v.action]
+  description = "Map of action values across all monitor_activity_log_alerts, keyed the same as var.monitor_activity_log_alerts"
+  value       = { for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : k => v.action }
 }
 output "monitor_activity_log_alerts_criteria" {
-  description = "List of criteria values across all monitor_activity_log_alerts"
-  value       = [for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : v.criteria]
+  description = "Map of criteria values across all monitor_activity_log_alerts, keyed the same as var.monitor_activity_log_alerts"
+  value       = { for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : k => v.criteria }
 }
 output "monitor_activity_log_alerts_description" {
-  description = "List of description values across all monitor_activity_log_alerts"
-  value       = [for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : v.description]
+  description = "Map of description values across all monitor_activity_log_alerts, keyed the same as var.monitor_activity_log_alerts"
+  value       = { for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : k => v.description }
 }
 output "monitor_activity_log_alerts_enabled" {
-  description = "List of enabled values across all monitor_activity_log_alerts"
-  value       = [for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : v.enabled]
+  description = "Map of enabled values across all monitor_activity_log_alerts, keyed the same as var.monitor_activity_log_alerts"
+  value       = { for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : k => v.enabled }
 }
 output "monitor_activity_log_alerts_location" {
-  description = "List of location values across all monitor_activity_log_alerts"
-  value       = [for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : v.location]
+  description = "Map of location values across all monitor_activity_log_alerts, keyed the same as var.monitor_activity_log_alerts"
+  value       = { for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : k => v.location }
 }
 output "monitor_activity_log_alerts_name" {
-  description = "List of name values across all monitor_activity_log_alerts"
-  value       = [for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : v.name]
+  description = "Map of name values across all monitor_activity_log_alerts, keyed the same as var.monitor_activity_log_alerts"
+  value       = { for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : k => v.name }
 }
 output "monitor_activity_log_alerts_resource_group_name" {
-  description = "List of resource_group_name values across all monitor_activity_log_alerts"
-  value       = [for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : v.resource_group_name]
+  description = "Map of resource_group_name values across all monitor_activity_log_alerts, keyed the same as var.monitor_activity_log_alerts"
+  value       = { for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : k => v.resource_group_name }
 }
 output "monitor_activity_log_alerts_scopes" {
-  description = "List of scopes values across all monitor_activity_log_alerts"
-  value       = [for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : v.scopes]
+  description = "Map of scopes values across all monitor_activity_log_alerts, keyed the same as var.monitor_activity_log_alerts"
+  value       = { for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : k => v.scopes }
 }
 output "monitor_activity_log_alerts_tags" {
-  description = "List of tags values across all monitor_activity_log_alerts"
-  value       = [for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : v.tags]
+  description = "Map of tags values across all monitor_activity_log_alerts, keyed the same as var.monitor_activity_log_alerts"
+  value       = { for k, v in azurerm_monitor_activity_log_alert.monitor_activity_log_alerts : k => v.tags }
 }
 
